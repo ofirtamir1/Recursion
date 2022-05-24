36801ab550f7520c0485f06eee4f6f538f097ffe
ofirtamir1 <106065255+ofirtamir1@users.noreply.github.com>
2022-05-24 11:16:47 +0300

    questions1-109

---
+++ answers.py
@@ -0,0 +1,17 @@
+#ex1
+def recur_sum(m):
+   if n <= 1:
+       return n
+   else:
+       return n + recur_sum(n-1)
+#ex2
+
+def Factorial(n):
+    if n == 1:
+        return 1
+    else:
+        return n * Factorial(n - 1)
+
+#print(Factorial(5))
+
+#ex3
